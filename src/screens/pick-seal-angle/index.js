7297c6a6a255399dc4d1157cd671a2d94f7cda03
Richard Frost <richard.frost@headforwards.com>
2019-09-18 15:57:00 +0100

    Only show 4 images

--- src/screens/pick-seal-angle/index.js
+++ src/screens/pick-seal-angle/index.js
@@ -3,33 +3,31 @@ import Footer from "../../components/footer";
 import "./pick-seal.css";
 
 class Upload extends Component {
-  left = [
+  angles = [
     {
       id: "wet-head-left",
       text: "Head left",
       img: "head-left.svg"
     },
     {
-      id: "bottling-left",
-      text: "Bottling Left",
-      img: "Seals_Icons-04.svg"
-    },
-    {
-      id: "lying-left",
-      text: "Lying left",
-      img: "lying-left.svg"
-    }
-  ];
-  right = [
-    {
       id: "wet-head-right",
       text: "Head Right",
       img: "head-right.svg"
     },
+    // {
+    //   id: "bottling-left",
+    //   text: "Bottling Left",
+    //   img: "Seals_Icons-04.svg"
+    // },
+    // {
+    //   id: "bottling-right",
+    //   text: "Bottling Right",
+    //   img: "Seals_Icons-04.svg"
+    // },
     {
-      id: "bottling-right",
-      text: "Bottling Right",
-      img: "Seals_Icons-04.svg"
+      id: "lying-left",
+      text: "Lying left",
+      img: "lying-left.svg"
     },
     {
       id: "lying-right",
@@ -54,18 +52,7 @@ class Upload extends Component {
       <>
         <div className="container">
           <div className="row justify-content-center">
-            {this.right.map(item => {
-              const selected = selectedHead === item.id ? "selected" : "";
-              return (
-                <div
-                  className={`col-5 head-card ${selected}`}
-                  onClick={() => this.selectHead(item.id)}
-                >
-                  <h2>{item.text}</h2> <img src={item.img} alt={item.text} />
-                </div>
-              );
-            })}
-            {this.left.map(item => {
+            {this.angles.map(item => {
               const selected = selectedHead === item.id ? "selected" : "";
               return (
                 <div
